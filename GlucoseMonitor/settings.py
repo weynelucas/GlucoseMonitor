27082add3b8377e323a284aca075390e42b6aff5
Lucas Weyne <weynelucas@gmail.com>
2016-08-09 17:54:56 -0300

    Deploy static files

--- GlucoseMonitor/settings.py
+++ GlucoseMonitor/settings.py
@@ -135,6 +135,9 @@ USE_L10N = True
 
 USE_TZ = False
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
