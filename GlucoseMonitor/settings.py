0366a135dbc8cfeeb40926fddfe9dc4688f5109d
Lucas Weyne <weynelucas@gmail.com>
2016-08-09 19:15:33 -0300

    Remove host email user

--- GlucoseMonitor/settings.py
+++ GlucoseMonitor/settings.py
@@ -167,5 +167,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'monitorglucose@gmail.com'
-EMAIL_HOST_PASSWORD = 'monitor##gluc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
