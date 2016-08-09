f8d64997cb9414b6f704b317562ce31d58f30baa
Lucas Weyne <weynelucas@gmail.com>
2016-08-09 19:18:34 -0300

    Host email user

--- GlucoseMonitor/settings.py
+++ GlucoseMonitor/settings.py
@@ -167,5 +167,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
